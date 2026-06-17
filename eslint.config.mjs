17bfcc63aff6beadd9cc04383d7069b18b632142
Tran Quang Ha <tranquangha2006@gmail.com>
2026-06-17 18:16:53 +0700

    chore(.husky): add .husky to prevent dirty commit & bad format before push

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['eslint.config.mjs', 'commitlint.config.cjs'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
