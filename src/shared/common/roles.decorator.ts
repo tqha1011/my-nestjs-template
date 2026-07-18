1acc1338aa22af98745818ddfe2b73da725bc8b5
Tran Quang Ha <tranquangha2006@gmail.com>
2026-07-18 22:54:21 +0700

    docs: update comments

--- src/shared/common/roles.decorator.ts
+++ src/shared/common/roles.decorator.ts
@@ -1,4 +1,4 @@
 import { Reflector } from '@nestjs/core';
 
-// default setting is string[], but high recommend to use enum type for better type safety
+// default setting is string[], but recommend to use enum type for better type safety
 export const Roles = Reflector.createDecorator<string[]>();
